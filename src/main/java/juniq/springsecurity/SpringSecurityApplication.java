4751ed04dd9d76e0c70b730f6882cd534fc5fb7f
juniq <juniqlim@gmail.com>
2023-03-05 15:07:34 +0900

    야호!

--- src/main/java/juniq/springsecurity/SpringSecurityApplication.java
+++ src/main/java/juniq/springsecurity/SpringSecurityApplication.java
@@ -2,6 +2,11 @@ package juniq.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -10,4 +15,13 @@ public class SpringSecurityApplication {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+            .username("admin")
+            .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+            .roles("USER", "ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
 }
